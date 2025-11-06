fe93153617892344c80ba581d64d0b80790991aa
GTX300RAPTOR <lahovk2@gmail.com>
2025-11-07 00:17:10 +0300

    Добавление README.md and test_processing.py

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -17,16 +17,37 @@ def test_filter_by_state() -> None:
 def test_sort_by_date() -> None:
     """Тест для проверки сортировки транзакций по дате."""
     sorted_transactions = sort_by_date(transactions)
-    assert sorted_transactions[0]['id'] == 3
+    # проверяем, что список отсортирован в обратном порядке по дате
+    assert sorted_transactions[0]['date'] == '2024-01-01T10:00:00'
+    assert sorted_transactions[1]['date'] == '2024-01-01T09:00:00'
+    assert sorted_transactions[2]['date'] == '2024-01-01T08:00:00'
 
 
 def test_sort_by_date_different_dates() -> None:
     """Тест для проверки сортировки транзакций с разными датами."""
     transactions_with_diff_dates = [
-      {'id': 1, 'state': 'EXECUTED', 'date': '2023-01-03T12:00:00'},
-      {'id': 2, 'state': 'CANCELED', 'date': '2023-01-01T13:00:00'},
-      {'id': 3, 'state': 'EXECUTED', 'date': '2023-01-02T14:00:00'}
+        {'id': 1, 'state': 'EXECUTED', 'date': '2023-01-03T12:00:00'},
+        {'id': 2, 'state': 'CANCELED', 'date': '2023-01-01T13:00:00'},
+        {'id': 3, 'state': 'EXECUTED', 'date': '2023-01-02T14:00:00'}
     ]
     sorted_transactions = sort_by_date(transactions_with_diff_dates)
 
-    assert sorted_transactions[0]['id'] == 1
+    # проверяем, что список отсортирован в обратном порядке по дате
+    assert sorted_transactions[0]['date'] == '2023-01-03T12:00:00'
+    assert sorted_transactions[1]['date'] == '2023-01-02T14:00:00'
+    assert sorted_transactions[2]['date'] == '2023-01-01T13:00:00'
+
+
+def test_sort_by_date_ascending() -> None:
+    """Тест для проверки сортировки транзакций по дате в возрастающем порядке."""
+    transactions_with_diff_dates = [
+        {'id': 1, 'state': 'EXECUTED', 'date': '2023-01-03T12:00:00'},
+        {'id': 2, 'state': 'CANCELED', 'date': '2023-01-01T13:00:00'},
+        {'id': 3, 'state': 'EXECUTED', 'date': '2023-01-02T14:00:00'}
+    ]
+    sorted_transactions = sort_by_date(transactions_with_diff_dates, reverse=False)
+
+    # Проверяем, что список отсортирован в возрастающем порядке по дате
+    assert sorted_transactions[0]['date'] == '2023-01-01T13:00:00'
+    assert sorted_transactions[1]['date'] == '2023-01-02T14:00:00'
+    assert sorted_transactions[2]['date'] == '2023-01-03T12:00:00'
